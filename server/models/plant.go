94ed86b16a9ea997b80a0abf87c267830c176906
Kyle Pekosh <kylep342@gmail.com>
2025-05-10 08:58:44 -0500

    Adds json keys for parsing for Plant struct

--- server/models/plant.go
+++ server/models/plant.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Plant struct {
-	Id          string      `db:"id"`
-	CultivarId  string      `db:"cultivar_id"`
-	SpeciesId   string      `db:"species_id"`
-	SeedId      string      `db:"seed_id"`
-	PollenId    string      `db:"pollen_id"`
-	Generation  uint32      `db:"generation"`
-	PlantedAt   time.Time   `db:"planted_at"`
-	HarvestedAt time.Time   `db:"harvested_at"`
-	Genetics    interface{} `db:"genetics"`
-	Labels      interface{} `db:"labels"`
+	Id          string      `db:"id" json:"id"`
+	CultivarId  string      `db:"cultivar_id" json:"cultivar_id"`
+	SpeciesId   string      `db:"species_id" json:"species_id"`
+	SeedId      string      `db:"seed_id" json:"seed_id"`
+	PollenId    string      `db:"pollen_id" json:"pollen_id"`
+	Generation  uint32      `db:"generation" json:"generation"`
+	PlantedAt   time.Time   `db:"planted_at" json:"planted_at"`
+	HarvestedAt time.Time   `db:"harvested_at" json:"harvested_at"`
+	Genetics    interface{} `db:"genetics" json:"genetics"`
+	Labels      interface{} `db:"labels" json:"labels"`
 }
 
 func (p *Plant) GetID() string { return p.Id }
